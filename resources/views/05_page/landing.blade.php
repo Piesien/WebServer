77060934de80debdb808bcb56bc72772e9ad2a87
jurisruseckis <juris.ruseckis@catchsmart.com>
2018-09-29 01:29:56 +0300

    Add responsive img to landing page

--- resources/views/05_page/landing.blade.php
+++ resources/views/05_page/landing.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <div class="hero">
+        @include('01_atom/responsive-img', [
+            'img' => [
+                'aspect' => '56%',
+                'url' => 'https://via.placeholder.com/1600x600',
+                'alt' => 'hero'
+            ]
+        ])
         @include('01_atom/choice-btn', [
             'btn' => [
                 'url' => '/map',
