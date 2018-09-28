41397bfc5fa1e2c4c3a086e4447e15bd10ea14cd
Osvalds Neiders <osvalds.neiders@gmail.com>
2018-09-29 01:47:06 +0300

    Render pins from firebase

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,12 +7,37 @@
 
 require('./bootstrap');
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAyD1hhGZ21d25MXX3FVEmOkK9rhpP_8PI",
+    authDomain: "piesien-6634e.firebaseapp.com",
+    databaseURL: "https://piesien-6634e.firebaseio.com",
+    projectId: "piesien-6634e",
+    storageBucket: "piesien-6634e.appspot.com",
+    messagingSenderId: "773809530700"
+};
+
 var map;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: {lat: 56.9496, lng: 24.1052},
         zoom: 8
     });
-}
+};
+
+firebase.initializeApp(config);
 initMap();
+
+var spots = firebase.database().ref('spots');
+
+spots.on('value', function(snapshot) {
+    let fspots = snapshot.val();
+    let markers = fspots.map(({latitude, longitude, id}) => {
+        return new google.maps.Marker({
+            position: {lat: latitude, lng: longitude},
+            label: id,
+            map: map
+        });
+    });
+});
--- resources/views/04_template/default.blade.php
+++ resources/views/04_template/default.blade.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title>piesien.lv</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
@@ -8,7 +9,7 @@
     @yield('content')
 </div>
 </body>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCUtMxSNXZYS3RPeQ_4NWAXHHXmP3IpSVU&callback=initMap"
-        async defer></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCUtMxSNXZYS3RPeQ_4NWAXHHXmP3IpSVU&callback=initMap"></script>
+<script src="https://www.gstatic.com/firebasejs/5.5.2/firebase.js"></script>
 <script src="{{ mix('js/app.js') }}"></script>
 </html>
