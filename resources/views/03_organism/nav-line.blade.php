ede943a3d219e48f816771fd73b431e7f5072294
jurisruseckis <juris.ruseckis@catchsmart.com>
2018-09-29 17:21:26 +0300

    Change logo

--- resources/views/03_organism/nav-line.blade.php
+++ resources/views/03_organism/nav-line.blade.php
@@ -1,7 +1,7 @@
 <div class="fixed-top">
     <ul class="nav nav-line">
         <li class="nav-item">
-            <a class="nav-link active" href="/"><img src="https://via.placeholder.com/100x50"></a>
+            <a class="nav-link active" href="/"><img src="/img/logo.png" width="50"></a>
         </li>
     </ul>
 </div>
