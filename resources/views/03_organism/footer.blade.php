b0979d0cf8b5396a5bb217d68edaf09fc66491eb
jurisruseckis <juris.ruseckis@catchsmart.com>
2018-09-29 03:18:33 +0300

    Update landing page

---
+++ resources/views/03_organism/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                {{ $data['copyrights'] }}
+            </div>
+        </div>
+    </div>
+</footer>
