28b20fc997424c9f2b399662d5170b2d4ce55960
jurisruseckis <juris.ruseckis@catchsmart.com>
2018-09-29 03:45:54 +0300

    Add meta tags

--- resources/views/04_template/default.blade.php
+++ resources/views/04_template/default.blade.php
@@ -2,6 +2,16 @@
 <head>
     <title>piesien.lv</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta property="og:title" content="piesien.lv" />
+    <meta property="og:url" content="piesien.lv" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="Lorem ipsum og description" />
+    <meta property="og:image" content="ogimage.jpg" />
+
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
